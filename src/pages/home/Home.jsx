a06c11aa23db41c49ab644f09f9a82feeacc90ca
Ankit <ankit0038973@gmail.com>
2023-11-24 12:22:42 +0530

    remove error in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./Trending/Trending";
-import Popular from "./Popular/Popular";
 import TopRated from "./topRated/TopRated";
+import Popular from "./popular/Popular";
 
 const Home = () => {
   return (
